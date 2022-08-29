d3b5c901051b753b667f1a24b445a2157c1a7f19
VinicioJesus <viniciojesus298@gmail.com>
2022-08-29 17:01:28 -0300

    creating the decorator of Login

--- backend/src/utils/decorators/FieldValidation.decorator.ts
+++ backend/src/utils/decorators/FieldValidation.decorator.ts
@@ -1,27 +1,50 @@
+import { SimpleConsoleLogger } from "typeorm";
 import BusinessLogicError from "../../handler/BusinessLogicError ";
 import errors from "../../handler/errors.handler";
 
 export const IsRequired = () => {
-  return (target: any, property: string) => {
-    const _property = target.constructor.props[property];
+  return (target : Object, property : string) => {
+    const original = target[property];
+    let _property : typeof original
 
-    if (_property === undefined || _property === null)
-      throw new BusinessLogicError({
-        errorMessage: `Field '${property}' can not be null!`,
-        errorCode: errors.NULL_FIELD.code,
-      });    
-  };    
+    const getter = () => _property
+
+    const setter = (value: any) => {
+      console.log('param value', value)
+
+      if (value === undefined || value === null)
+                throw new BusinessLogicError({
+                    errorMessage : `Field '${property}' can not be null!`,
+                    errorCode : errors.NULL_FIELD.code
+                })
+
+      // original.set(value)
+
+      console.log(original)
+
+      console.log('real value', value)
+
+    }
+
+    Object.defineProperty(target, property, {
+        get: getter,
+        set: setter
+    });
+}   
 };
+
+
 
 export const IsInEnum = (_enum: object) => {
   return (target: any, property: string) => {
     
-    const _property = target.constructor.props[property];
+    // const _property = target.constructor.props[property];
 
-    if (Object.values(_enum).indexOf(_property) === -1 )
-      throw new BusinessLogicError({
-        errorMessage: `Feld '${property}' value is not valid'!`,
-        errorCode: errors.BASE.code,
-      });
+    // if (Object.values(_enum).indexOf(_property) === -1 )
+    //   throw new BusinessLogicError({
+    //     errorMessage: `Field '${property}' value is not valid'!`,
+    //     errorCode: errors.BASE.code,
+    //   });
+    //return console.log(target + "IsEnum");
   };
 };
