98fdde5b430311215dd0c58d3f5b07d80a4b70fc
REBECA LOPES DE OLIVEIRA <21276568@dsn127m1242267.cfp127aluno.local>
2022-08-30 15:43:33 -0300

    add landing page

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,16 +2,24 @@ import { Text, View } from "react-native"
 import FeedbackTextInput from "../../components/FeedbackInput";
 import styles from "./style" ;
 import ButtonComponent from "../../components/Button";
+import colors from "../../styles/colors";
 
 const LandingPage : React.FC = () => {
 
     return (
          <View style={styles.container}>
-            <Text>DevInterection</Text>
+            <Text style={styles.logo}>DevInterection</Text>
+            <View style={styles.main}>
+                <Text style={styles.welcome}>Seja Bem Vindo</Text>
+                <Text style={styles.message}>Dev Int conecta todos no processo de aprimoramento pessoal e em equipe </Text>
+            </View>
+            <View>
+                <ButtonComponent text="login" onPress={() => {}}/>
+                <ButtonComponent text="cadastrar" onPress={() => {}}></ButtonComponent>
+            </View>
         </View>
+        
     )
 }
-
-styles
 
 export default LandingPage;
--- mobile/src/pages/Home/style.ts
+++ mobile/src/pages/Home/style.ts
@@ -1,14 +1,41 @@
 import { StyleSheet } from "react-native";
 import colors from "../../styles/colors";
+import fonts from "../../styles/typography";
+import globalStyles from "../../styles/global";
 
-export default StyleSheet.create({
+const style = StyleSheet.create({
     container : {
         backgroundColor : colors.BLACK,
         width: '100%',
         height: '100%',
         display: 'flex',
         flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logo : {
+        color: colors.WHITE,
+        fontSize: 32,
+        marginBottom: 56
+    },
+    main : {
+        width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
-    }
+
+    },
+    welcome : {
+        color: colors.PRIMARY,
+        fontSize: 24,
+        fontFamily: fonts.POPPINS_BOLD,
+        marginBottom: 40,
+    },
+    message : {
+        ...globalStyles.text,
+
+        color: colors.WHITE,
+        marginBottom: 56
+    },
 })
+
+export default style
