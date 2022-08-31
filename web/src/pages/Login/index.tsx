203b7035e585afad6fa486a707e47703d064f3f1
EZEQUIEL MATHIAS DE OLIVEIRA PEREIRA <21276325@dsn127m1241959.cfp127aluno.local>
2022-08-31 16:59:26 -0300

    add login page

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -9,23 +9,18 @@ const LoginPage: React.FC = () => {
             <header><h1>Bem vindo</h1></header>
 
             <main>
+                <div className="login-container">
                 <h2>Entrar</h2>
                 <Input icon="email" placeholder={"Senha"} onChange={() => { }} />
                 <Input icon="lock" placeholder={"Email"} onChange={() => { }} />
                 <a href="#">Esqueci minha senha</a>
                 <Button>Login</Button>
 
-                <div className="teste">
-                
-
-                    
-                <Button className="logins">
-
-                </Button>
-
-                <Button className="logins">
-
-                </Button>
+                <div className="logins">
+                 
+                <hr /><span>ou</span><hr />
+               
+                </div>
                 </div>
             </main>
         </div>
