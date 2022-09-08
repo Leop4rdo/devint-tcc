80817ff12573c81c9fd04a8bc9c8b0fd0e78aa65
Leonardo Antunes <leonardoantunes1401@gmail.com>
2022-09-08 16:26:02 -0300

    add animations

--- mobile/src/components/register/forms/Step1.tsx
+++ mobile/src/components/register/forms/Step1.tsx
@@ -1,5 +1,6 @@
 import {useEffect, useRef} from "react"
-import { View, Text, Animated } from "react-native"
+import { View, Text, Animated, Easing } from "react-native"
+import useAnimation from "../../../hooks/useAnimation"
 import FeedbackTextInput from "../../utils/FeedbackInput"
 
 
@@ -10,21 +11,14 @@ export interface IRegisterFormProps {
 }
 
 const RegisterFormStep1 : React.FC<IRegisterFormProps> = ({styles, formData, onChange}) => {
-    const fadeInAnimation = useRef(new Animated.Value(0)).current
+    const opacityAnim = useAnimation(0, 1)
 
     useEffect(() => {
-        Animated.timing(
-            fadeInAnimation,
-            {
-                toValue: 1,
-                useNativeDriver : true,
-                duration: 750
-            }
-        ).start()
-    }, [fadeInAnimation])
+        opacityAnim.start();
+    }, [opacityAnim.prop])
 
     return (
-        <Animated.View style={{...styles.form, opacity : fadeInAnimation}}>
+        <Animated.View style={{...styles.form, opacity : opacityAnim.prop}}>
             <FeedbackTextInput style={styles.input} placeholder="Digite seu nome" icon="person" onChangeText={(text : string) => onChange(text, "name")} ></FeedbackTextInput>
 
             <FeedbackTextInput style={styles.input} placeholder="Digite um email" icon="mail" onChangeText={(text : string) => onChange(text, "email")} ></FeedbackTextInput>
--- mobile/src/components/register/forms/Step2.tsx
+++ mobile/src/components/register/forms/Step2.tsx
@@ -1,4 +1,6 @@
-import { View, Text } from "react-native"
+import { useEffect } from "react"
+import { View, Text, Animated } from "react-native"
+import useAnimation from "../../../hooks/useAnimation"
 import FeedbackTextInput from "../../utils/FeedbackInput"
 import { IRegisterFormProps } from "./Step1"
 
@@ -6,14 +8,20 @@ import { IRegisterFormProps } from "./Step1"
 
 
 const RegisterFormStep2 : React.FC<IRegisterFormProps> = ({styles, formData, onChange}) => {
+    const opacityAnim = useAnimation(0, 1)
+
+    useEffect(() => {
+        opacityAnim.start();
+    }, [opacityAnim.prop])
+
     return(
-        <View style={styles.form}>
+        <Animated.View style={{...styles.form, opacity : opacityAnim.prop}}>
             <FeedbackTextInput 
                 style={styles.input} 
                 placeholder="" 
                 icon="calendar-today" 
                 onChangeText={(text : string) => onChange(text, 'birthday') } ></FeedbackTextInput>
-        </View>
+        </Animated.View>
     )
 }
 
