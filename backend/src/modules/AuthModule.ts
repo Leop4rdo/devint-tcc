3671ee977821c3386a51815694fea801fa10bbbc
Leonardo Antunes <leonardoantunes1401@gmail.com>
2022-09-12 16:57:13 -0300

    add profile pic and bio github integration

--- backend/src/modules/AuthModule.ts
+++ backend/src/modules/AuthModule.ts
@@ -1,7 +1,10 @@
 import CompanyEntity from "../core/entities/CompanyEntity";
 import DevEntity from "../core/entities/DevEntity";
 import IAuthService from "../core/services/abstract/IAuthService";
+import { IDevService } from "../core/services/abstract/IDevService";
+import IService from "../core/services/abstract/IService";
 import AuthService from "../core/services/concrete/AuthService";
+import DevService from "../core/services/concrete/DevService";
 import IAuthRepository from "../infra/repositories/abstract/IAuthRepository";
 import ICompanyRepository from "../infra/repositories/abstract/ICompanyRepository";
 import IDevRepository from "../infra/repositories/abstract/IDevRepository";
@@ -14,6 +17,7 @@ export default class AuthModule {
     private authRepo : IAuthRepository
     private devRepo : IDevRepository
     private companyRepo : ICompanyRepository
+    private devService : IDevService
 
     private authService : IAuthService
 
@@ -21,8 +25,9 @@ export default class AuthModule {
         this.authRepo = new AuthRepository();
         this.devRepo = new DevRepository();
         this.companyRepo = new CompanyRepository();
+        this.devService = new DevService(this.devRepo);
         
-        this.authService = new AuthService(this.authRepo, this.devRepo, this.companyRepo);
+        this.authService = new AuthService(this.authRepo, this.devRepo, this.companyRepo, this.devService);
     }
 
     getAuthService = () => this.authService
