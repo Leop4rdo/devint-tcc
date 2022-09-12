8b44374134e2dd38fb6e2e1646cd2a17cfc67449
Mariana Gomes <marianabicla@gmail.com>
2022-09-12 00:04:50 -0300

    feat: :lipstick: styling and fixing buttons and links

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "components/utils/Input"
-import { Button } from "@mui/material";
+import Button from "components/utils/Button";
 import { Link } from 'react-router-dom'
 import LogoComponent from "components/utils/Logo";
+import Icon from "components/utils/Icon";
 
 const LoginPage: React.FC = () => {
+
+    const [passwordShown, setPasswordShown] = useState(false);
+
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown);
+    }
 
     return (
         <div className="login-page">
             <header>
-                <h1><LogoComponent secondary="#1F252F" primary="#7865FF"/></h1> 
+                <h1><LogoComponent secondary="#1F252F" primary="#7865FF" /></h1>
             </header>
 
             <main>
@@ -18,22 +25,32 @@ const LoginPage: React.FC = () => {
                 </div>
                 <div className="login-wrapper">
                     <div className="login-container">
+
                         <h2>Entrar</h2>
-                        <Input icon="email" placeholder={"Senha"} onChange={() => { }} type="email" />
-                        <Input icon="lock" placeholder={"Email"} onChange={() => { }} type="password" />
+
+                        <Input icon="email" placeholder="E-mail" onChange={() => { }} type="email" />
+
+                        <div className="password-container">
+
+                            <Input icon="lock" placeholder="Senha" onChange={() => { }} type={passwordShown ? "text" : "password"} />
+
+                            <Button className="btn-toggle-password" onClick={togglePassword} children={<Icon name={passwordShown ? "visibility_off" : "visibility"} />}></Button>
+
+                        </div>
+
                         <a href="#">Esqueci minha senha</a>
 
-                        <Button className="button-login">Login</Button>
+                        <Button className="btn-primary">Login</Button>
 
                         <p>ou</p>
 
                         <div className="button-container">
-                            <Button className="login-pairing"> <img src="assets/icons/github.svg" alt="" /></Button>
-                            <Button className="login-pairing" >
+                            <Button className="login-pairing btn-primary"> <img src="assets/icons/github.svg" alt="" /></Button>
+                            <Button className="login-pairing btn-primary" >
                                 <img src="assets/icons/google.svg" alt="" />
                             </Button>
                         </div>
-                            <Link className="container-new-user" to='/register'>Sou novo aqui</Link>
+                        <Link className="container-new-user" to='/register'>Sou novo aqui</Link>
 
                     </div>
                 </div>
