e16803795710435a7cdf663d48690b711921b76d
Mariana Gomes <marianabicla@gmail.com>
2022-09-14 00:07:38 -0300

    testing new logic

--- web/src/components/utils/Icon/index.tsx
+++ web/src/components/utils/Icon/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 interface IIconProps {
     name : string
+    onClick? : any
 }
 
-const Icon: React.FC<IIconProps> = ({name}) => {
+const Icon: React.FC<IIconProps> = ({name, onClick}) => {
     return (
-        <span className="icon material-symbols-rounded">
+        <span className="icon material-symbols-rounded" onClick={onClick}>
             {name}
         </span>
     )
