3b9c3611746ba396e94b010b565c7b3d38fdea27
Leonardo Antunes <leonardoantunes@esn127d1007477.sp.local>
2022-10-03 11:36:36 -0300

    add object mapper to utils

--- backend/src/utils/ObjectMapper.ts
+++ backend/src/utils/ObjectMapper.ts
@@ -1,9 +1,12 @@
-const ObjectMapper = (target : Object, source : Object) => {
-    for (const key of Object.keys(source)) {
-        const val = source[key];
-        if (val !== undefined) {
-            target[key] = val;
-        }
-    }
-    return target 
+const ObjectMapper = (target: Object, source: Object) => {
+    const filteredEntries = Object.entries(source)
+        .filter(([key, value ]) => Object.keys(target).includes(key))
+
+    // for (const [key, value] of filteredEntries) {
+    //     target[key as keyof typeof target] = value;
+    // }
+    
+    filteredEntries.forEach(([key, value]) => target[key] = value)
+
+    return target
 }
