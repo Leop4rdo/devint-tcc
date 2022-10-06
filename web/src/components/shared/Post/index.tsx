1b01a2b7fc44be0367cae2ca653c92518073f115
Mariana Gomes <marianabicla@gmail.com>
2022-10-05 22:41:54 -0300

    minor changes

--- web/src/components/shared/Post/index.tsx
+++ web/src/components/shared/Post/index.tsx
@@ -7,28 +7,35 @@ interface IPostProps {
     data : IPost
 }
 
-
 const Post: React.FC<IPostProps> = ({ data }) => {
     
+    let commentAuthors = data.comments.map(function(item){
+        return item.writter.profilePicUrl;
+    })
+    
+    commentAuthors.length = 2;
+
     return (
         <div className="postcard" key={data.id}>
             <div className="post-header">
                 <div className="user-info">
                     <img src={data.writter.profilePicUrl} />
-                    <h4>{data.writter.name}</h4>
+                    <h2>{data.writter.name}</h2>
                 </div>
                 <Button children={[<Icon name="done"/> , "Seguir"]}/>
             </div>
 
-            <div>
+            <div className="post-content">
                 <p>{data.content}</p>
                 <div className="post-images">
-                    {data.attachments}
+                    <img src={data.attachments[0]} />
                 </div>
             </div>
             <div className="post-footer">
                 <div className="comments">
-                    <img src={commentData.slice(0, 3)} />
+                    <img src={commentAuthors[0]} />
+                    <img src={commentAuthors[1]} />
+                    <span>10 comentários</span>
                 </div>
                 <div className="hearts">
                     {data.hearts}
