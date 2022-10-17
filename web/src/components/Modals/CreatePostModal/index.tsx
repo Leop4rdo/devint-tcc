b236494dbfe9e32048d0106a991e1ddcc1290330
Leonardo Antunes <leonardoantunes1401@gmail.com>
2022-10-17 16:57:56 -0300

    f

--- web/src/components/Modals/CreatePostModal/index.tsx
+++ web/src/components/Modals/CreatePostModal/index.tsx
@@ -1,16 +1,41 @@
 import Icon from "components/shared/Icon";
 import AutoTextArea from "components/shared/TextArea";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "store/context/Auth.context";
-
+import firebase from "config/firebase"
 interface ICreatePostModalProps {
     onClose : () => void
 }
 
 const CreatePostModal : React.FC<ICreatePostModalProps> = ({ onClose }) => {
     const authContext = useContext(AuthContext)
+    const [content, setContent] = useState('');
+    const [attachments, setAttachments] = useState<string[]>([])
+    const [uploading, setUploading] = useState(false);
 
-    console.log(authContext?.userData)
+    const upload = async (evt : any) => {
+        setUploading(true)
+
+        const uri = evt.target.files[0]
+
+        try {
+            const res = await fetch(uri)
+            const blob = await res.blob()
+            const fileName = uri.substring(uri.lastIndexOf('/')+1)
+
+            const uploaded = await firebase.storage().ref().child(fileName).put(blob)
+
+            setAttachments([
+                ...attachments,    
+                await uploaded.ref.getDownloadURL()
+            ])
+        } catch (err) {
+            console.log(err)
+            alert('Houve um erro inesperado ao fazer upload!')
+        }
+
+        setUploading(false)
+    }
 
     return (
         <div className="modal-wrapper">
@@ -28,18 +53,30 @@ const CreatePostModal : React.FC<ICreatePostModalProps> = ({ onClose }) => {
                 </header>
 
                 <div className="content">
-                    <AutoTextArea className="post-content-textarea" placeholder="ex : hoje estou pensando em aprender..."/>
+                    <AutoTextArea onChange={({target}) => setContent(target.value)}  className="post-content-textarea" placeholder="ex : hoje estou pensando em aprender..."/>
+                
+                    <div className="attachment-list">
+                        {
+                            attachments.map((uri, idx) => 
+                                <img src={uri} key={idx}/>
+                            )
+                        }
+                    </div>
                 </div>
 
                 <footer>
                     <div className="attachment-load-options">
-                        <Icon name="image" />
+                        <div>
+                            <input accept="image/*" type="file" name="attachment-input" id="attachment-input" />
+                            <label htmlFor="attachment-input"><Icon name="image" /></label>
+                        </div>
+
                     </div>
                     <div className="post-options">
                         <select>
                             <option selected>Selecione um projeto</option>
                         </select>
-                        <button className="btn-primary publish-btn">
+                        <button className={`btn-primary publish-btn ${content || uploading ? '' : 'disabled'}`}>
                             Publicar
                         </button>
                     </div>
