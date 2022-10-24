d5e99e9d16fdf4bbc952979cef09b3bc8a83b01f
EZEQUIEL MATHIAS DE OLIVEIRA PEREIRA <21276325@dsn127m1241959.cfp127aluno.local>
2022-10-24 16:55:09 -0300

    Starting profile page

--- web/src/pages/private/UserProfile/index.tsx
+++ web/src/pages/private/UserProfile/index.tsx
@@ -18,6 +18,18 @@ const UserProfilePage: React.FC = () => {
                         <div className="container-image-face-user">
                         </div>
                         <h2>Name user</h2>
+                        <p>Bio muito bunita feita para exemplificar uns bagui ai
+                            tipo... alguma coisa</p>
+
+                        <div className="container-followers">
+                            <div>
+                            <span>43</span>
+                            <p>Seguidores</p>
+                            
+                            </div>
+                            <span>45</span>
+                            <p>Seguidores</p>
+                        </div>
                     </div>
 
 
