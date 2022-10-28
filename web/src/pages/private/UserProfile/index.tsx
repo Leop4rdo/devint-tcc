a0083c487620d3850e0bc8a3882f297dee2089b6
Mariana Santos <mariana-m.santos@thalesgroup.com>
2022-10-28 11:57:31 -0300

    layout changes

--- web/src/pages/private/UserProfile/index.tsx
+++ web/src/pages/private/UserProfile/index.tsx
@@ -9,134 +9,125 @@ const UserProfilePage: React.FC = () => {
 
     return (
         <MenuWapper>
-            <div className="container-user-profile">
+            <div className="profile-page">
 
-                <div className="container-profile-style">
+                <div className="background-image"></div>
 
-                    <div className="background-image">
+                <div className="container-user-info">
+
+                    <div className="main-profile-info">
+
+                        <Icon name="edit" />
+
+                        <div className="container-image-face-user">
+                            <img />
+                        </div>
+
+                        <h2>Name user</h2>
+
+                        <span>
+                            <img src="assets/icons/github.svg" alt="" />
+                            Ezequiel-Mathias
+                        </span>
+
+                        <p>Bio muito bunita feita para exemplificar uns bagui ai
+                            tipo... alguma coisa</p>
+
+                        <div className="container-follower-data">
+                            <div className="container-followers">
+                                <span>43</span>
+                                <p>Seguidores</p>
+
+                            </div>
+                            <div className="container-following">
+                                <span>45</span>
+                                <p>Seguindo</p>
+                            </div>
+
+                        </div>
+
+                        <div className="container-follow-unfollow">
+                            <span><Icon name="add" />Seguir</span>
+                        </div>
+
                     </div>
 
-
-                    <div className="container-of-information">
-
-                        <div className="container-profile-data">
-                            <div className="icon-edit">
-                                <Icon name="edit" />
-                            </div>
-                            <div className="container-image-face-user">
-
+                    <UserProfileEdit iconName="group" subject="Contatos">
+                        
+                            <div className="container-email">
+                                <Icon name="email" />
+                                <span>emailqualddddddddddddddddddquer@gmail.com</span>
                             </div>
 
-                            <div className="data-user">
-                                <h2>Name user</h2>
-                                <span>
-                                    <img src="assets/icons/github.svg" alt="" />
-                                    Ezequiel-Mathias
-                                </span>
-                                <p>Bio muito bunita feita para exemplificar uns bagui ai
-                                    tipo... alguma coisa</p>
+                            <div className="container-phone">
+                                <Icon name="call" />
+                                <span>(11) 4954-5965</span>
                             </div>
 
-                            <div className="container-follower-data">
-                                <div className="container-followers">
-                                    <span>43</span>
-                                    <p>Seguidores</p>
+                    </UserProfileEdit>
 
+                    <UserProfileEdit iconName="center_focus_weak" subject="Foco de carreira" >
+
+                        <div className="container-career-focus">
+                            <span>Front-End</span>
+                        </div>
+
+                    </UserProfileEdit>
+
+
+                    <UserProfileEdit iconName="school" subject="Senioridade">
+                        <div className="container-seniority-user">
+                            <span>Junior</span>
+                        </div>
+                    </UserProfileEdit>
+
+
+                    <UserProfileEdit iconName="star" subject="Habilidades">
+                        <div className="skill-user">
+                            <div className="container-skill-user">
+                                <div className="container-skill">
+                                    <Icon name="star" />
+                                    <span>Html e css</span>
                                 </div>
-                                <div className="container-following">
-                                    <span>45</span>
-                                    <p>Seguindo</p>
-                                </div>
-
                             </div>
 
-                            <div className="container-follow-unfollow">
-                                <span><Icon name="add"/>Seguir</span>
+                            <div className="container-skill-user">
+                                <div className="container-skill">
+                                    <Icon name="star" />
+                                    <span>Html e css</span>
+                                </div>
+                            </div>
+                            <div className="container-skill-user">
+                                <div className="container-skill">
+                                    <Icon name="star" />
+                                    <span>Html e css</span>
+                                </div>
                             </div>
                         </div>
 
-                        <UserProfileEdit iconName="group" subject="Contatos">
-                            <div className="container-contact-quite">
-                                <div className="container-email">
-                                    <Icon name="email" />
-                                    <span>emailqualddddddddddddddddddquer@gmail.com</span>
-                                </div>
+                    </UserProfileEdit>
 
-                                <div className="container-phone">
-                                    <Icon name="call" />
-                                    <span>(11) 4954-5965</span>
-                                </div>
 
+                    <UserProfileEdit iconName="push_pin" subject="Outros links">
+
+                        <div className="container-links">
+                            <div className="links">
+                                <span>Link qualquer</span>
                             </div>
 
-                        </UserProfileEdit>
-
-                        <UserProfileEdit iconName="center_focus_weak" subject="Foco de carreira" >
-
-                            <div className="container-career-focus">
-                                <span>Front-End</span>
+                            <div className="links">
+                                <span>Link qualquer</span>
                             </div>
 
-                        </UserProfileEdit>
-
-
-                        <UserProfileEdit iconName="school" subject="Senioridade">
-                            <div className="container-seniority-user">
-                                <span>Junior</span>
+                            <div className="links">
+                                <span>Link qualquer</span>
                             </div>
-                        </UserProfileEdit>
-
-
-                        <UserProfileEdit iconName="star" subject="Habilidades">
-                            <div className="skill-user">
-                                <div className="container-skill-user">
-                                    <div className="container-skill">
-                                        <Icon name="star" />
-                                        <span>Html e css</span>
-                                    </div>
-                                </div>
-
-                                <div className="container-skill-user">
-                                    <div className="container-skill">
-                                        <Icon name="star" />
-                                        <span>Html e css</span>
-                                    </div>
-                                </div>
-                                <div className="container-skill-user">
-                                    <div className="container-skill">
-                                        <Icon name="star" />
-                                        <span>Html e css</span>
-                                    </div>
-                                </div>
+                            <div className="links">
+                                <span>Link qualquer</span>
                             </div>
+                        </div>
 
-                        </UserProfileEdit>
-
-
-                        <UserProfileEdit iconName="push_pin" subject="Outros links">
-
-                            <div className="container-links">
-                                <div className="links">
-                                    <span>Link qualquer</span>
-                                </div>
-
-                                <div className="links">
-                                    <span>Link qualquer</span>
-                                </div>
-
-                                <div className="links">
-                                    <span>Link qualquer</span>
-                                </div>
-                                <div className="links">
-                                    <span>Link qualquer</span>
-                                </div>
-
-
-                            </div>
-
-                        </UserProfileEdit>
-
-                    </div>
+                    </UserProfileEdit>
 
                 </div>
 
