d9b54fe7341802114e1a88ee9791e03d9a60eca3
Leonardo Antunes <leonardoantunes1401@gmail.com>
2022-11-04 14:01:01 -0300

    fix ignore auth option

--- backend/src/application/middlewares/auth.middleware.ts
+++ backend/src/application/middlewares/auth.middleware.ts
@@ -2,9 +2,22 @@ import { NextFunction, Request, Response } from "express";
 import BadRequestResponse from "../Responses/BadRequestResponse";
 import * as jwt from "jsonwebtoken"
 import forbiddenResponse from "../Responses/ForbiddenResponse";
+import DevRepository from "@src/adapters/database/repositories/DevRepository";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "@src/adapters/database/data-source";
+import DevEntity from "@src/adapters/database/entities/DevEntity";
+import DevOutput from "@src/ports/output/user/DevOutput";
+
 
 const authMiddleware = (req : Request, res : Response, next : NextFunction) => {
-    if (process.env.PROFILE === 'dev' && process.env.IGNORE_AUTH) return next()
+    if (process.env.PROFILE === 'dev' && process.env.IGNORE_AUTH) {
+        AppDataSource.getRepository<DevEntity>(DevEntity).find().then(devs => {
+            req.body.userData =new DevOutput(devs[0])
+            return next()
+        });
+
+        return
+    }
 
     const auth = req.get("Authorization")
 
