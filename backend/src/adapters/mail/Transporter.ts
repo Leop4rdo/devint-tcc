167925f6bbe6dfa4b3a9219ba83c4eacb642be86
Leonardo Antunes <leonardoantunes1401@gmail.com>
2022-11-16 22:27:59 -0300

    fix backend env

---
+++ backend/src/adapters/mail/Transporter.ts
@@ -0,0 +1,16 @@
+import { createTransport } from "nodemailer";
+
+const transporter = createTransport({
+    auth: {
+        user : process.env.MAIL_USER,
+        pass : process.env.MAIL_PASSWORD
+    },
+    host: "smtp-mail.outlook.com",
+    secure: false,
+    port : 587,
+    tls : {
+        ciphers : 'SSLv3'
+    }
+} ) 
+
+export default transporter
