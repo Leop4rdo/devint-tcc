64dcdd3e3ee7d3320e8d2a3d3599b4c2659876d1
Leonardo Antunes <leonardoantunes1401@gmail.com>
2022-11-21 16:41:05 -0300

    pushzão de cria na develop

--- mobile/src/pages/private/ProjectRegister/style.ts
+++ mobile/src/pages/private/ProjectRegister/style.ts
@@ -1,6 +1,7 @@
 import { StyleSheet } from "react-native";
 import colors from "../../../styles/colors";
 import fonts from "../../../styles/typography";
+import { screenHeight } from "../../../styles/utils";
 
 export default StyleSheet.create({
     page : {
@@ -35,4 +36,68 @@ export default StyleSheet.create({
         fontSize : 18,
     },
 
+    formWrapper : {
+        width : "100%",
+        minHeight : screenHeight,
+        padding : 16,
+    },
+
+    bannerContainer : {
+        position : 'relative',
+        width : "100%",
+        aspectRatio : 3,
+        backgroundColor : colors.GRAY,
+    },
+
+    addBannerBtn : {
+        width : 32,
+        height : 32,
+
+        backgroundColor : colors.PRIMARY,
+
+        position: 'absolute',
+        right : 4,
+        bottom : 4,
+
+        justifyContent : 'center',
+        alignItems : 'center',
+
+        borderRadius : 1000
+    },
+
+    divisor : {
+        width : '100%',
+        height : 2,
+        backgroundColor : colors.GRAY,
+        marginVertical : 16,
+    },
+
+    input : {
+        width : "100%",
+        marginHorizontal : 0,
+        marginVertical : 0,
+        marginBottom : 16
+    },
+
+    descInput : {
+        alignItems : 'flex-start',
+        minHeight : 160,
+        paddingVertical : 12,
+    },
+
+    label : {
+        fontFamily : fonts.POPPINS_REGULAR,
+        color : colors.LIGHT_GRAY,
+        marginRight : 16
+    },
+
+    addBtn : {
+        height : 48,
+        marginLeft : 8,
+        aspectRatio : 1,
+        borderRadius : 16,
+        backgroundColor : colors.PRIMARY,
+        alignItems : 'center',
+        justifyContent : 'center'
+    }
 })
